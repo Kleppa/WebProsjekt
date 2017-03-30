c013de4e141eb0ca8f219874f1451bf0b38d7b8d
Kleppa <jarandwaagekleppa@gmail.com>
2017-03-30 10:57:55 +0200

    init

--- src/index.php
+++ src/index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
     <meta charset="UTF-8">
     <title>Title</title>
 
