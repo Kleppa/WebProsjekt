c41dfd9b2cc32cd358c4521a564c748892d3c467
Kleppa <jarandwaagekleppa@gmail.com>
2017-03-30 11:25:34 +0200

    init

--- src/index.php
+++ src/index.php
@@ -4,10 +4,24 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/main.css">
     <meta charset="UTF-8">
+
     <title>Title</title>
+
 
 </head>
 <body>
+<div class="col-lg-9 col-md-8 col-sm-6 col-xs-2">
+
+<div id="testDiv" class = jumbotron container-fluid>
+    <h1>This is a test<h2>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-8 col-sm-6 col-xs-2">
+<div id="testDiv" class = jumbotron container-fluid>
+    <h1>This is a test<h2>
+</div>
+</div>
 
 </body>
 </html>
