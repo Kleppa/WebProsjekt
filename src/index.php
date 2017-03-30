d731d89d139f2f218cab80a7d2c7d562b7db7b03
Kleppa <jarandwaagekleppa@gmail.com>
2017-03-30 12:13:15 +0200

    its a start

--- src/index.php
+++ src/index.php
@@ -10,11 +10,15 @@
 
 </head>
 <body>
-<nav class="navbar fixed-top navbar-inverse bg-primary">
+<nav class="navbar fixed-top navbar-inverse bg-primary container-fluid" id="navbar">
     <div class="col-xs-6"><h1 id="name">NameOfWebPage</h1></div>
     <div class="col-xs-6"><h1 id="name">topic</h1></div>
 
 </nav>
+<div class="row">
+    <div class="col-lg-7 col-md-2 col-md-offset-3">
+        <img src="20100902_img-test.jpg">
+</div>
 
 
 </body>
