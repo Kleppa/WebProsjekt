54881892e02f8e72473b4e71ef0dcab1afa976af
Kleppa <jarandwaagekleppa@gmail.com>
2017-03-30 11:28:28 +0200

    init

--- src/index.php
+++ src/index.php
@@ -10,18 +10,7 @@
 
 </head>
 <body>
-<div class="col-lg-9 col-md-8 col-sm-6 col-xs-2">
 
-<div id="testDiv" class = jumbotron container-fluid>
-    <h1>This is a test<h2>
-</div>
-</div>
-
-<div class="col-lg-4 col-md-8 col-sm-6 col-xs-2">
-<div id="testDiv" class = jumbotron container-fluid>
-    <h1>This is a test<h2>
-</div>
-</div>
 
 </body>
 </html>
