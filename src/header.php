2a81c3955b4ad01e5a37e2b6782cc2e00fd16d15
bareHenrik <henriklg@gmail.com>
2017-04-05 14:29:55 +0200

    Lagde header.php

---
+++ src/header.php
@@ -0,0 +1,14 @@
+<script src ="css/js/dropdown.js"></script>
+<nav class="navbar fixed-top navbar-inverse bg-primary" id="navbar">
+    <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" style="background-color: #000000" type="button" data-toggle="dropdown">Meny
+            <span class="caret"></span></button>
+        <ul class="dropdown-menu">
+            <li><a href="#">drinks</a></li>
+            <li><a href="#">events</a></li>
+            <li><a href="#">map</a></li>
+        </ul>
+    </div>
+    <h1 class="col-md-offset-5" id="name">Wenture</h1>
+
+</nav>
--- src/index.php
+++ src/index.php
@@ -12,20 +12,9 @@
 </head>
 <body>
 
-<script src ="css/js/dropdown.js"></script>
-<nav class="navbar fixed-top navbar-inverse bg-primary" id="navbar">
-    <div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle" style="background-color: #000000" type="button" data-toggle="dropdown">Meny
-            <span class="caret"></span></button>
-        <ul class="dropdown-menu">
-            <li><a href="#">drinks</a></li>
-            <li><a href="#">events</a></li>
-            <li><a href="#">map</a></li>
-        </ul>
-    </div>
-  <h1 class="col-md-offset-5" id="name">Wenture</h1>
-
-</nav>
+<?php
+require "header.php";
+?>
 
 <div class="row c">
     <div class="col-lg-6 col-md-2 col-md-offset-3" id="centerDiv">
