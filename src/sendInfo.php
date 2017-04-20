6edbf3ddad8386ffede33a9d1c3442296e041c03
Kleppa <jarandwaagekleppa@gmail.com>
2017-04-20 12:52:09 +0200

    variables get/post not working

--- src/sendInfo.php
+++ src/sendInfo.php
@@ -1,8 +1,9 @@
 <?php
 require 'phpscripts/db_connector.php';
-echo "TEST";
-if (isset($_POST['user']) && isset($_POST['pass'])) {
-    echo "TEST";
+echo "required php passed";
+echo $_GET['user'];
+if (isset($_POST["user"]) && isset($_POST["pass"])) {
+    echo "Ifcheck passed";
     $sql = "SELECT username, password FROM Users";
     $result = $conn->query($sql);
 
