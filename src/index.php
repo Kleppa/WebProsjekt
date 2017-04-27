c5d369d78d101cde85f119f5a42d121e9fe67bc0
bareHenrik <henriklg@gmail.com>
2017-04-27 13:11:05 +0200

    La til karusell og ny header

--- src/index.php
+++ src/index.php
@@ -1,34 +1,94 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/main.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/mdl/material.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+    <link rel="stylesheet" href="css/bootstrap-4/css/bootstrap.min.css">
+
+
     <title>Wenture</title>
 
 
 </head>
 <body>
+<?php require "header.php"; ?>
 
-<?php
-require "header.php";
-?>
 
-<div class="row">
-    <div class="col-lg-6 col-md-2 col-md-offset-3" id="centerDiv">
-        <img src="20100902_img-test.jpg">
-    </div>
-</div>
-<!--Fix posistioning and scaleability -->
-<div class="imageContainer">
+        <div class="container">
 
-    <!-- setup clickable images from database-->
+            <h2>Carousel</h2>
+
+            <p>The Carousel creates a rotating slide show.</p>
+
+            <div id="karusell" class="carousel" data-ride="carousel">
+
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                </ol>
+
+                <div class="carousel-inner" role="listbox">
+
+                    <div class="carousel-item active">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/d/d3/Arnaut_and_his_dog_by_Jean_Leon_gerome.jpg" alt="...">
+                        <div class="carousel-caption">
+                            <h3>Arnaut Blowing Smoke in His Dog's Nose,</h3>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item-next">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Bristol_Museum_Tadema_unconscious_rivals.JPG" alt="...">
+                        <div class="carousel-caption">
+                            Unconscious Rivals, Lawrence Alma-Tadema
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/c/c0/Ferdinand_Max_Bredt_-_Türkische_Frauen.jpg" alt="...">
+                        <div class="carousel-caption">
+                            Turkish Women, Ferdinand Max Bredt
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/4/46/Leon_Cogniet_-_L_Expedition_D_Egypte_Sous_Les_Ordres_De_Bonaparte.jpg" alt="...">
+                        <div class="carousel-caption">
+                            The 1798 Egyptian Expedition Under the Command of Bonaparte, Léon Cogniet, Louvre
+                        </div>
+                    </div>
+
+                </div>
+
+                <!-- Controls -->
+                <a class="carousel-control-prev" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+
+                <a class="carousel-control-next" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="aaaaaa" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <br>
+            <p>You can create a slideshow of images.  It works best if they are all the same size, but these images from Wikipedia vary a bit, so the slide size varies.  The carousel is sizeable depending on page width.</p>
+
+        </div>
+
+
+    <!-- setup clickable images from database -->
 
     <div class="imageHolder" id="drinkImg"><a href=""><!--Image--></a> </div>
     <div class="imageHolder" id="TasteImg"><a href=""><!--Image--></a></div>
     <div class="imageHolder" id="StreetImg"><a href=""><!--Image--></a></div>
     <div class="imageHolder" id="chillImg"><a href=""><!--Image--></a></div>
 </div>
+
+
 <div class="txtContainerIndex">
     <p class="btnTxt" id="drinkText">Drinks</p>
     <p class="btnTxt" id="tasteTxt">Taste</p>
@@ -37,6 +97,12 @@ require "header.php";
 
 </div>
 
+<?php require "footer.php"; ?>
+
+<!-- Scripts -->
+<script src="css/bootstrap-4/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script src="css/mdl/material.min.js"></script>
 
 </body>
 </html>
