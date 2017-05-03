fd3ea9d442a8124a9a6c382a0628da7a54567b5b
bareHenrik <henriklg@gmail.com>
2017-05-03 13:53:54 +0200

    fikset på index

--- src/footer.php
+++ src/footer.php
@@ -1,13 +1,13 @@
 <div id="footer">
-<footer class="mdl-mini-footer">
-    <div class="mdl-mini-footer__left-section">
-        <div class="mdl-logo">Title</div>
-        <ul class="mdl-mini-footer__link-list">
-            <li><a href="#">Help</a></li>
-            <li><a href="#">Privacy & Terms</a></li>
-        </ul>
-    </div>
-</footer>
+    <footer class="mdl-mini-footer">
+        <div class="mdl-mini-footer__left-section">
+            <div class="mdl-logo">Title</div>
+            <ul class="mdl-mini-footer__link-list">
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Privacy & Terms</a></li>
+            </ul>
+        </div>
+    </footer>
 
 </div>
 
