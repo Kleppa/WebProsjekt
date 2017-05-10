619e692aaad4c1163e6eeb1be7dd30e8ae09f50a
danieldalseth <danieldalseth@yahoo.no>
2017-05-10 13:16:28 +0200

    test underside

---
+++ src/undersider/Døgnvill underside.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<header>
+
+<link rel="stylesheet" src="../css/bootstrap-4/css/bootstrap.css"> </link>
+    <link rel="stylesheet" src="../css/bootstrap-4/css/bootstrap-grid.css"> </link>
+    <link rel="stylesheet" src="../css/bootstrap-4/css/bootstrap-rebot.css"> </link>
+    <link rel="stylesheet" src="./css/main.css"> </link>
+
+<body>
+
+<?php require '../header.php'; ?>
+
+<div id="fasade-døgnvill"> Bilde av bar / resturant som blir til kart. </div>
+
+<h3 id="Spisested-døgnvill">Døgnvill - Bar og burger</h3>
+
+<h3 id="meny-døgnvill">Oversikt | Spesial | Kontakt</h3>
+
+<p id="fine-ord-døgnvill">
+    Døgnvill - Bar og burger byr på god mat og drikke. Prøv de forskjellige burgerene, eller om du bare ønsker deg en milkshake, har de det og.
+</p>
+
+<h3 id="andre-steder-døgnvill">Andre steder i området</h3>
+
+<div id="sted1-døgnvill"></div>
+<div id="sted2-døgnvill"></div>
+<div id="sted3-døgnvill"></div>
+
+
+</body>
+</html>
