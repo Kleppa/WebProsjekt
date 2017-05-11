10556177c8d954925a6c39e692482f7b52a51899
danieldalseth <danieldalseth@yahoo.no>
2017-05-11 15:34:16 +0200

    Jobb med underside og changes

--- src/undersider/restaurant_info.php
+++ src/undersider/restaurant_info.php
@@ -2,26 +2,21 @@
 
     <div class="container">
 
-
         <div class="row justify-content-center">
             <div class="col-9">
                 <img class="img-fluid" id="bildeboks" src="http://placehold.it/900x300"> <!--Bildet skal fetches -->
             </div>
             </div>
 
-
         <div class="row justify-content-center"> <h3> Navn på sted </h3> </div>
 
-
         <div class="row justify-content-center"> <h5> Oversikt | Spesial | Kontakt </h5> </div>
-
 
         <div class="row justify-content-center">
             <div class="col-8">
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-            </div>
+            </div> <!-- Her skal det stå pene ord om stedet.-->
         </div>
-
 
         <div class="row justify-content-center"> <h6> Andre steder i området </h6> </div>
 
@@ -39,11 +34,9 @@
                 <img class="img-fluid" src="http://placehold.it/200x300">
             </div>
 
-
+            <!-- Alle bildene her skal fetches -->
 
         </div>
-
-
 
     </div> <!-- container -->
 
