80e2b1a672a80184e55e520ca632d6ece7c0878b
danieldalseth <danieldalseth@yahoo.no>
2017-05-11 11:43:23 +0200

    Jobb med underside og changes

--- src/undersider/restaurant_info.php
+++ src/undersider/restaurant_info.php
@@ -1,21 +1,20 @@
 <?php require '../header.php'; ?>
 
+    <div class="container">
 
-<div id="fasade-døgnvill"> Bilde av bar / resturant med knapp som gjør om til til kart. </div>
+        <div> Bilde av bar / resturant med knapp som gjør om til til kart.</div>
 
-<h3 id="Spisested-døgnvill">Døgnvill - Bar og burger</h3>
+        <h3>Døgnvill - Bar og burger</h3>
 
-<h3 id="meny-døgnvill">Oversikt | Spesial | Kontakt</h3>
+        <h3>Oversikt | Spesial | Kontakt</h3>
 
-<p id="fine-ord-døgnvill">
-    Døgnvill - Bar og burger byr på god mat og drikke. Prøv de forskjellige burgerene, eller om du bare ønsker en milkshake, har de det og.
-</p>
+        <p>
+            Døgnvill - Bar og burger byr på god mat og drikke. Prøv de forskjellige burgerene, eller om du bare ønsker
+            en milkshake, har de det og.
+        </p>
 
-<h3 id="andre-steder-døgnvill">Andre steder i området</h3>
+        <h3>Andre steder i området</h3>
 
-<div id="sted1-døgnvill"></div>
-<div id="sted2-døgnvill"></div>
-<div id="sted3-døgnvill"></div>
-
+    </div> <!-- container -->
 
 <?php require '../footer.php';
