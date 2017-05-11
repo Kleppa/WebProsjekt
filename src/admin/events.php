ddab772c85a3cbc08c76330caccdcad4f822251f
Kleppa <jarandwaagekleppa@gmail.com>
2017-05-11 15:12:05 +0200

    sql query fixing

--- src/admin/events.php
+++ src/admin/events.php
@@ -4,13 +4,13 @@ require $_SERVER['DOCUMENT_ROOT'].'/src/private/phpscripts/db_connector.php';
 require $_SERVER['DOCUMENT_ROOT'].'/src/private/phpscripts/functions.php';
 
 $pagetitle = 'Events';
-require '../header.php'; ?>
+ require '../header.php'; ?>
 
     <!-- content -->
     <div class="container">
         <div class="card-columns mt-4">
             <?php
-            $sql = "SELECT * FROM events;";
+            $sql = "SELECT events.*,types.type FROM events LEFT JOIN types ON types.id = events.type;";
 
             if ($result = $mysqli->query($sql)) {
                 foreach ($result as $row) {
