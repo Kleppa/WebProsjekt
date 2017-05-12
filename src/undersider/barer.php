f72f2bc010496fb5514ce0132491c2cfc0af2887
B1tm4n <knut.a.abelseth@live.no>
2017-05-12 16:46:38 +0200

    undertabs for places

--- src/undersider/barer.php
+++ src/undersider/barer.php
@@ -1,29 +1,76 @@
 <?php require "../header.php" ?>
 
-<div class="headline"><h1>Hytta Bar</h1></div>
+    <div class="margin-adder">
+    <div class="container mt-5">
+        <div class="container">
 
-<div class="tab">
-    <button class="tablinks" onclick="openCity(event, 'London')">Fine Ord</button>
-    <button class="tablinks" onclick="openCity(event, 'Paris')">Spesialiter</button>
-    <button class="tablinks" onclick="openCity(event, 'Tokyo')">Kontakt Oss</button>
-</div>
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <img class="img-fluid" id="bildeboks" src="http://placehold.it/900x300"> <!--Bildet skal fetches -->
+                </div>
+            </div>
 
-<div id="London" class="tabcontent">
-    <p>Hytta Bar er en fin og attraktiv plass der man føler seg som på hytta. og ja, vi ha utedass.</p>
-</div>
+            <div class="row justify-content-center">
+                <div class="col-sm-10">
 
-<div id="Paris" class="tabcontent">
-    <p>- Chill bar</p>
-    <p>- Billig HyttePils</p>
-    <p>- Kidza sitter igjen hjemme</p>
-</div>
+            <ul class="nav nav-tabs">
 
-<div id="Tokyo" class="tabcontent">
-    <p>- Tlf: 999 99 999 </p>
-    <p>- Adresse: Veien 1, 0000 Oslo </p>
-    <p>- E-Post: post@post.no</p>
-</div>
+                <li class="nav-item">
+                    <a href="#info" class="nav-link active" role="tab" data-toggle="tab">Fine Orde</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#spesial" class="nav-link" role="tab" data-toggle="tab">Spesialiter</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#kontakt" class="nav-link" role="tab" data-toggle="tab">Kontakt Oss</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#kart" class="nav-link" role="tab" data-toggle="tab">Kart</a>
+                </li>
+
+            </ul>
 
 
 
+            <div class="tab-content">
+                <div role="tabpanel" class="tab-pane active" id="info">1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                </div>
+                <div role="tabpanel" class="tab-pane" id="spesial">2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                </div>
+                <div role="tabpanel" class="tab-pane" id="kontakt">3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                </div>
+                <div role="tabpanel" class="tab-pane" id="kart">4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                </div>
+            </div>
+
+                </div>
+            </div>
+
+
+        <div class="row justify-content-center">
+            <h6> Andre steder i området </h6> </div>
+        <!--Bilder skal fetches-->
+
+        <div class="row justify-content-center text-center">
+
+            <div class="col-3">
+                <img class="img-fluid" src="http://placehold.it/200x300">
+            </div>
+
+            <div class="col-3">
+                <img class="img-fluid" src="http://placehold.it/200x300">
+            </div>
+
+            <div class="col-3">
+                <img class="img-fluid" src="http://placehold.it/200x300">
+            </div>
+
+            <!-- Alle bildene her skal fetches -->
+
+        </div>
+        </div>
+            <script src="../css/js/tests/tabsBar.js"></script>
 <?php require "../footer.php";
--- src/undersider/restaurant_info.php
+++ src/undersider/restaurant_info.php
@@ -21,16 +21,44 @@
 
         <div class="row justify-content-center"> <h3> Navn på sted </h3> </div>
 
-        <div class="row justify-content-center"> <h5> Oversikt | Spesial | Kontakt </h5> </div>
 
         <div class="row justify-content-center">
-            <div class="col-8">
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-            </div> <!-- Her skal det stå pene ord om stedet, blir fethcet fra databasen.-->
+            <div class="col-sm-10">
+
+                <ul class="nav nav-tabs">
+
+                    <li class="nav-item">
+                        <a href="#info" class="nav-link active" role="tab" data-toggle="tab">Fine Orde</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#spesial" class="nav-link" role="tab" data-toggle="tab">Spesialiter</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#kontakt" class="nav-link" role="tab" data-toggle="tab">Kontakt Oss</a>
+                    </li>
+
+
+                </ul>
+
+
+
+                <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane active" id="info">1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="spesial">2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="kontakt">3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                    </div>
+                </div>
+
+            </div>
         </div>
 
-        <div class="row justify-content-center"> <h6> Andre steder i området </h6> </div>
 
+        <div class="row justify-content-center">
+            <h6> Andre steder i området </h6> </div>
         <!--Bilder skal fetches-->
 
         <div class="row justify-content-center text-center">
@@ -50,7 +78,8 @@
             <!-- Alle bildene her skal fetches -->
 
         </div>
+    </div>
+    <script src="../css/js/tests/tabsBar.js"></script>
 
-    </div> <!-- container -->
 
 <?php require '../footer.php';
