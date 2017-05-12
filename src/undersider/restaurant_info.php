c497902a6c6024aa95879895f9b9fdca9be644de
danieldalseth <danieldalseth@yahoo.no>
2017-05-12 12:17:30 +0200

    Jobb med underside og changes

--- src/undersider/restaurant_info.php
+++ src/undersider/restaurant_info.php
@@ -15,10 +15,12 @@
         <div class="row justify-content-center">
             <div class="col-8">
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-            </div> <!-- Her skal det stå pene ord om stedet.-->
+            </div> <!-- Her skal det stå pene ord om stedet, blir fethcet fra databasen.-->
         </div>
 
         <div class="row justify-content-center"> <h6> Andre steder i området </h6> </div>
+
+        <!--Bilder skal fetches-->
 
         <div class="row justify-content-center text-center">
 
