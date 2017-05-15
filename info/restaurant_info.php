dfc209ccd438714978650fb083f3c5ee559d7381
B1tm4n <knut.a.abelseth@live.no>
2017-05-15 15:39:01 +0200

    php funksjon for informasjon på tabs.

--- info/restaurant_info.php
+++ info/restaurant_info.php
@@ -1,6 +1,27 @@
-<?php require '../header.php'; ?>
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/private/phpscripts/db_connector.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/private/phpscripts/functions.php';
+
+$pagetitle = 'Restaurant';
+
+if(!(isset($_GET['id']))){
+    redirect('/places.php');
+}
+
+require '../header.php'; ?>
 
     <div class="container margin-adder">
+        <div class="row">
+            <?php
+            $sql = "SELECT * FROM places WHERE id = {$_GET['id']};";
+            $row = "";
+            if ($result = $mysqli->query($sql)) {
+                foreach ($result as $value) {
+                    $row = $value;
+                }
+            } ?>
+        </div> <!-- card-columns -->
 
         <div class="row justify-content-center">
             <div class="col-9">
@@ -17,8 +38,9 @@
 
                 <!-- Tab panes -->
                 <div class="tab-content">
-                    <div role="tabpanel" class="tab-pane  in active" id="profile"><img class="img-fluid" id="bildeboks"
-                                                                                       src="http://placehold.it/900x300">
+                    <div role="tabpanel" class="tab-pane  in active" id="profile">
+                        <img class="img-fluid" id="bildeboks"
+                             src="<?php echo $row['image_path']; ?>"/>
                     </div>
                     <div role="tabpanel" class="tab-pane fade" id="buzz">bbb</div>
 
@@ -27,7 +49,8 @@
             </div>
         </div>
 
-        <div class="row justify-content-center"><h3> Navn på sted </h3></div>
+        <div class="row justify-content-center"><h3 class="card-title">
+                <?php echo $row['name'] ?></h3></div>
 
 
         <div class="row justify-content-center">
@@ -52,23 +75,23 @@
 
 
                 <div class="tab-content">
-                    <div role="tabpanel" class="tab-pane active" id="info"><p>1 Lorem ipsum dolor sit amet, consectetur
+                    <div role="tabpanel" class="tab-pane active" id="info">
+                        <p class="card-text"><?php echo $row['description'] ?></p>
+                    </div>
+
+                    <div role="tabpanel" class="tab-pane" id="spesial">
+                        <p>2 Lorem ipsum dolor sit amet, consectetur
                             adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
                             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                             fugiat nulla pariatur. </p>
                     </div>
-                    <div role="tabpanel" class="tab-pane" id="spesial"><p>2 Lorem ipsum dolor sit amet, consectetur
-                            adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-                            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                            fugiat nulla pariatur. </p>
-                    </div>
-                    <div role="tabpanel" class="tab-pane" id="kontakt"><p>3 Lorem ipsum dolor sit amet, consectetur
-                            adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-                            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                            fugiat nulla pariatur. </p>
+                    <div role="tabpanel" class="tab-pane" id="kontakt">
+                        <p class="card-text">Adresse: <?php echo $row['address']; ?></p>
+                        <p class="card-text">Telefon: <?php echo $row['phone']; ?></p>
+                        <p class="card-text">Åpningstider: <?php echo $row['opening_hours']; ?></p>
+                        <p class="card-text">Nettsted: <?php echo $row['url']; ?></p>
+
                     </div>
                 </div>
 
