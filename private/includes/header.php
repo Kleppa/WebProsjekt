a9d14d86d9700495af059871d7bd72f2f36c756e
Kleppa <jarandwaagekleppa@gmail.com>
2017-05-16 16:25:36 +0200

    working on adding trending places, sizing problems atm

--- private/includes/header.php
+++ private/includes/header.php
@@ -43,7 +43,7 @@
 
     </div><!--Container-->
     <?php
-    if (loggedIn()) {
+    if (!loggedIn()) {
         echo '<div class="pull-right float-button circle" id="key">
 <i class="material-icons" id="vpnKey">vpn_key</i> </div>';
     }
