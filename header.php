ff07fb8a1c0576a7c13dbef28355cd3a7520d1ad
Kleppa <jarandwaagekleppa@gmail.com>
2017-05-16 13:49:46 +0200

    added loggin btn in header

--- header.php
+++ header.php
@@ -1,11 +1,14 @@
 <!doctype html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Wenture<?php if (isset($pagetitle)) {
+    <title>Wenture<?php
+
+        if (isset($pagetitle)) {
             echo ' - ' . ucfirst($pagetitle);
         } ?></title>
 
@@ -40,9 +43,10 @@
 
     </div><!--Container-->
     <?php
+    require "private/phpscripts/functions.php";
     if(!loggedIn()) {
-        echo '<div class="pull-right float-button circle" id="key">
-    <i class="material-icons" id="vpnKey">vpn_key</i> </div>';
+        echo '<a href="#"> <div class="pull-right float-button circle" id="key">
+    <i class="material-icons" id="vpnKey">vpn_key</i> </div></a>';
     }
     ?>
 </nav><!--Navbar -->
