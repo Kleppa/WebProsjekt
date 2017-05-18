69c6767d09fbad92f3dff7ee0181b86db916d9fc
Kleppa <jarandwaagekleppa@gmail.com>
2017-05-18 14:53:29 +0200

    Lagt til huskelister....

--- index.php
+++ index.php
@@ -91,6 +91,16 @@ require_once 'private/includes/header.php';  // Header
         </div>
 
         <div class="row">
+            <!-- Bildene maa gjores klikkbare!
+            <a href="<?php/*
+
+                $sql = "SELECT *  FROM events ORDER BY score DESC  LIMIT 1 ;";
+                if ($result = $mysqli->query($sql)) {
+                    echo 'info/restaurant_info?id='.$result['id'].'"';
+                }
+
+            */?>
+            -->
             <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12">
                 <img class="mb-3"   <?php
 
@@ -99,7 +109,7 @@ require_once 'private/includes/header.php';  // Header
                     foreach ($result as $row){
                         echo 'src="'.$row['image_path'] . '"'.' alt="'.$row['title'].'"style="height:218px;width:218px"';
                     }
-                    echo 'style="min-width: 100%;">';
+                    echo 'style="min-width: 100%;min-height:100%">';
                     echo '<h4>'.$row['title'].'</h4>';
                     echo '<p>'.$row['description'].'</p>';
 
@@ -108,9 +118,17 @@ require_once 'private/includes/header.php';  // Header
                 ?>
             </div>
             <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12">
-                <img class="mb-3" src="http://placehold.it/200x200" alt="qwe" style="min-width: 100%;">
-                <h4>Place</h4>
-                <p>Keywords about the place</p>
+                <img class="mb-3"  <?php $sql = "SELECT *  FROM places ORDER BY score DESC  LIMIT 1 ;";
+                     if ($result = $mysqli->query($sql)) {
+                foreach ($result as $row){
+                echo 'src="'.$row['image_path'] . '"'.' alt="'.$row['name'].'"style="height:218px;width:218px"';
+                }
+                echo 'style="min-width: 100%;min-height:100%">';
+                echo '<h4>'.$row['name'].'</h4>';
+                echo '<p>'.$row['description'].'</p>';
+
+                }
+            ?>
             </div>
             <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12">
                 <img class="mb-3" src="http://placehold.it/200x200" alt="qwe" style="min-width: 100%;">
