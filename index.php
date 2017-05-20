c70e8984ddf82585cbb1fb2d84ebb4f5c8ad1a1f
Kleppa <jarandwaagekleppa@gmail.com>
2017-05-20 14:05:53 +0200

    added a tags to mainpage carussell

--- index.php
+++ index.php
@@ -19,63 +19,83 @@ require_once 'private/includes/header.php';  // Header
         <div class="carousel-inner" role="listbox">
 
             <div class="carousel-item active">
-                <img class="d-block img-fluid"
-                    <?php
-                    $sql = "SELECT *  FROM events ORDER BY score DESC  LIMIT 1 ;";
-                    if ($result = $mysqli->query($sql)) {
-                        foreach ($result as $row){
-                            echo 'src="'.$row['image_path'] . '"'.' alt="'.$row['title'].'"';
+                <a href="events.php">
+                    <img class="d-block img-fluid"
+                        <?php
+                        $sql = "SELECT *  FROM events ORDER BY score DESC  LIMIT 1 ;";
+                        if ($result = $mysqli->query($sql)) {
+                            foreach ($result as $row) {
+                                echo 'src="' . $row['image_path'] . '"' . ' alt="' . $row['title'] . '"';
                             }
 
                         }
-                    ?>>
+                        ?>>
 
-                <div class="carousel-caption">
-                    <h3>Barer</h3>
-                </div>
+                    <div class="carousel-caption">
+                        <h3>Barer</h3>
+                    </div>
             </div>
+            </a>
 
             <div class="carousel-item">
-                <img class="d-block img-fluid"
-                    <?php
-                    // bildet viser paa alle pcer utenom jarand sin pc........
-                    $sql = "SELECT *  FROM places ORDER BY score DESC  LIMIT 1 ;";
-                    if ($result = $mysqli->query($sql)) {
-                        foreach ($result as $row){
-                            echo 'src="'.$row['image_path'] . '"'.' alt="'.$row['name'].'"';
+                <a href="events.php?category=places">
+                    <img class="d-block img-fluid"
+                        <?php
+                        // bildet viser paa alle pcer utenom jarand sin pc........
+                        $sql = "SELECT *  FROM places ORDER BY score DESC  LIMIT 1 ;";
+                        if ($result = $mysqli->query($sql)) {
+                            foreach ($result as $row) {
+                                echo 'src="' . $row['image_path'] . '"' . ' alt="' . $row['name'] . '"';
+                            }
+
                         }
+                        ?>>
 
-                    }
-                    ?>>
-
-                <div class="carousel-caption">
-                    <h3>Steder</h3>
-                </div>
+                    <div class="carousel-caption">
+                        <h3>Steder</h3>
+                    </div>
+                </a>
             </div>
 
+
             <div class="carousel-item">
-                <img class="d-block img-fluid"
-                    <?php
+                <a href="events.php?category=drink">
+                    <img class="d-block img-fluid"
+                        <?php
 
-                    $sql = "SELECT *  FROM events ORDER BY score DESC  LIMIT 1 ;";
-                    if ($result = $mysqli->query($sql)) {
-                        foreach ($result as $row){
+                        $sql = "SELECT *  FROM events ORDER BY score DESC  LIMIT 1 ;";
+                        if ($result = $mysqli->query($sql)) {
+                            foreach ($result as $row) {
 
-                            echo 'src="'.$row['image_path'] . '"'.' alt="'.$row['title'].'"';
+                                echo 'src="' . $row['image_path'] . '"' . ' alt="' . $row['title'] . '"';
+                            }
+
                         }
-
-                    }
-                    ?>>
-                <div class="carousel-caption">
-                    <h3> The park</h3>
-                </div>
+                        ?>>
+                    <div class="carousel-caption">
+                        <h3> The park</h3>
+                    </div>
+                </a>
             </div>
 
             <div class="carousel-item">
-                <img class="d-block img-fluid" src="http://placehold.it/1000x600" alt="...">
-                <div class="carousel-caption">
-                    <h3>Concert this week!</h3>
-                </div>
+                <a href="events.php?category=food">
+                    <img class="d-block img-fluid"
+                        <?php
+
+                        $sql = "SELECT *  FROM food ORDER BY score DESC  LIMIT 1 ;";
+                        if ($result = $mysqli->query($sql)) {
+                            foreach ($result as $row) {
+
+                                echo 'src="' . $row['image_path'] . '"' . ' alt="' . $row['name'] . '"';
+                            }
+
+                        }
+                        ?>>
+                    <div class="carousel-caption">
+                        <h3>Concert this week!</h3>
+                    </div>
+                </a>
             </div>
 
 
@@ -95,120 +115,118 @@ require_once 'private/includes/header.php';  // Header
     </div>
 
     <div class="container" id="trending-wrapper">
-        <div class="row">
-            <div class="col">
-                <h2>Trending places right now:</h2>
-                <p>See the most popular places by category</p>
-            </div>
+    <div class="row">
+        <div class="col">
+            <h2>Trending places right now:</h2>
+            <p>See the most popular places by category</p>
         </div>
+    </div>
 
-        <div class="row">
-            <a style="color: black; text-decoration: none;"href= <?php
-            $sql = "SELECT *  FROM events ORDER BY score DESC  LIMIT 1 ;";
-            if ($result = $mysqli->query($sql)) {
-                foreach ($result as $row) {
-                    echo '"info/restaurant_info.php?id=' . $row['id'] . '"';
-                }
+    <div class="row">
+        <a style="color: black; text-decoration: none;" href= <?php
+        $sql = "SELECT *  FROM events ORDER BY score DESC  LIMIT 1 ;";
+        if ($result = $mysqli->query($sql)) {
+            foreach ($result as $row) {
+                echo '"info/restaurant_info.php?id=' . $row['id'] . '"';
             }
+        }
 
 
-            ?>>
+        ?>>
             <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12">
-                <img class="mb-3"   <?php
+                <img class="mb-3" <?php
 
                 $sql = "SELECT *  FROM events ORDER BY score DESC  LIMIT 1 ;";
                 if ($result = $mysqli->query($sql)) {
-                    foreach ($result as $row){
-                        echo 'src="'.$row['image_path'] . '"'.' alt="'.$row['title'].'"style="height:218px;width:218px"';
+                    foreach ($result as $row) {
+                        echo 'src="' . $row['image_path'] . '"' . ' alt="' . $row['title'] . '"style="height:218px;width:218px"';
                     }
                     echo 'style="min-width: 100%;min-height:100%">';
-                    echo '<h4>'.$row['title'].'</h4>';
-                    echo '<p>'.$row['description'].'</p>';
+                    echo '<h4>' . $row['title'] . '</h4>';
+                    echo '<p>' . $row['description'] . '</p>';
 
                 }
 
                 ?>
             </div>
-            </a>
-            <a style="color: black; text-decoration: none;"href= <?php
-            $sql = "SELECT *  FROM places ORDER BY score DESC  LIMIT 1 ;";
-                     if ($result = $mysqli->query($sql)) {
-                         foreach ($result as $row) {
-                             echo '"info/restaurant_info.php?id=' . $row['id'] . '"';
-                         }
-                     }
+        </a>
+        <a style="color: black; text-decoration: none;" href= <?php
+        $sql = "SELECT *  FROM places ORDER BY score DESC  LIMIT 1 ;";
+        if ($result = $mysqli->query($sql)) {
+            foreach ($result as $row) {
+                echo '"info/restaurant_info.php?id=' . $row['id'] . '"';
+            }
+        }
 
 
-            ?>>
+        ?>>
             <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12">
 
-                <img class="mb-3"  <?php $sql = "SELECT *  FROM places ORDER BY score DESC  LIMIT 1 ;";
-                     if ($result = $mysqli->query($sql)) {
-                foreach ($result as $row){
-                echo 'src="'.$row['image_path'] . '"'.' alt="'.$row['name'].'"style="height:218px;width:218px"';
-                }
-                echo 'style="min-width: 100%;min-height:100%">';
-                echo '<h4>'.$row['name'].'</h4>';
-                echo '<p>'.$row['description'].'</p>';
+                <img class="mb-3" <?php $sql = "SELECT *  FROM places ORDER BY score DESC  LIMIT 1 ;";
+                if ($result = $mysqli->query($sql)) {
+                    foreach ($result as $row) {
+                        echo 'src="' . $row['image_path'] . '"' . ' alt="' . $row['name'] . '"style="height:218px;width:218px"';
+                    }
+                    echo 'style="min-width: 100%;min-height:100%">';
+                    echo '<h4>' . $row['name'] . '</h4>';
+                    echo '<p>' . $row['description'] . '</p>';
 
                 }
-            ?>
+                ?>
             </div>
-            </a>
-            <a style="color: black; text-decoration: none;"href= <?php
-            $sql = "SELECT *  FROM places ORDER BY score DESC  LIMIT 1,1 ;";
-            if ($result = $mysqli->query($sql)) {
-                foreach ($result as $row) {
-                    echo '"info/restaurant_info.php?id=' . $row['id'] . '"';
-                }
+        </a>
+        <a style="color: black; text-decoration: none;" href= <?php
+        $sql = "SELECT *  FROM places ORDER BY score DESC  LIMIT 1,1 ;";
+        if ($result = $mysqli->query($sql)) {
+            foreach ($result as $row) {
+                echo '"info/restaurant_info.php?id=' . $row['id'] . '"';
             }
+        }
 
 
-            ?>>
-                <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12">
+        ?>>
+            <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12">
 
-                    <img class="mb-3"  <?php $sql = "SELECT *  FROM places ORDER BY score DESC  LIMIT 1,1 ;";
-                    if ($result = $mysqli->query($sql)) {
-                        foreach ($result as $row){
-                            echo 'src="'.$row['image_path'] . '"'.' alt="'.$row['name'].'"style="height:218px;width:218px"';
-                        }
-                        echo 'style="min-width: 100%;min-height:100%">';
-                        echo '<h4>'.$row['name'].'</h4>';
-                        echo '<p>'.$row['description'].'</p>';
-
+                <img class="mb-3" <?php $sql = "SELECT *  FROM places ORDER BY score DESC  LIMIT 1,1 ;";
+                if ($result = $mysqli->query($sql)) {
+                    foreach ($result as $row) {
+                        echo 'src="' . $row['image_path'] . '"' . ' alt="' . $row['name'] . '"style="height:218px;width:218px"';
                     }
-                    ?>
-                </div>
-            </a>
+                    echo 'style="min-width: 100%;min-height:100%">';
+                    echo '<h4>' . $row['name'] . '</h4>';
+                    echo '<p>' . $row['description'] . '</p>';
 
-
-
-
-            <a style="color: black; text-decoration: none;"href= <?php
-            $sql = "SELECT *  FROM events ORDER BY score DESC  LIMIT 1,1 ;";
-            if ($result = $mysqli->query($sql)) {
-                foreach ($result as $row) {
-                    echo '"info/restaurant_info.php?id=' . $row['id'] . '"';
                 }
+                ?>
+            </div>
+        </a>
+
+
+        <a style="color: black; text-decoration: none;" href= <?php
+        $sql = "SELECT *  FROM events ORDER BY score DESC  LIMIT 1,1 ;";
+        if ($result = $mysqli->query($sql)) {
+            foreach ($result as $row) {
+                echo '"info/restaurant_info.php?id=' . $row['id'] . '"';
             }
+        }
 
 
-            ?>>
-                <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12">
+        ?>>
+            <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12">
 
-                    <img class="mb-3"  <?php $sql = "SELECT *  FROM events ORDER BY score DESC  LIMIT 1,1 ;";
-                    if ($result = $mysqli->query($sql)) {
-                        foreach ($result as $row){
-                            echo 'src="'.$row['image_path'] . '"'.' alt="'.$row['title'].'"style="height:218px;width:218px"';
-                        }
-                        echo 'style="min-width: 100%;min-height:100%">';
-                        echo '<h4>'.$row['title'].'</h4>';
-                        echo '<p>'.$row['description'].'</p>';
-
+                <img class="mb-3" <?php $sql = "SELECT *  FROM events ORDER BY score DESC  LIMIT 1,1 ;";
+                if ($result = $mysqli->query($sql)) {
+                    foreach ($result as $row) {
+                        echo 'src="' . $row['image_path'] . '"' . ' alt="' . $row['title'] . '"style="height:218px;width:218px"';
                     }
-                    ?>
-                </div>
-            </a>
+                    echo 'style="min-width: 100%;min-height:100%">';
+                    echo '<h4>' . $row['title'] . '</h4>';
+                    echo '<p>' . $row['description'] . '</p>';
+
+                }
+                ?>
+            </div>
+        </a>
     </div>
 
 <?php require_once 'private/includes/footer.php';
