0005be69494bcb7e5c6a60e1dcea5ab7486f10ad
Even Gabrielsen <gabreve16@student.westerdals.no>
2017-05-26 16:19:25 +0200

    Lagt inn flere felter på modify_event formet og fikset form prosesseringen.

--- private/form_processors/save_event.php
+++ private/form_processors/save_event.php
@@ -8,20 +8,24 @@ if (isset($_POST['submit'])) {
 
     $title = mysqlPrep($_POST['title']);
     $desc = mysqlPrep($_POST['description']);
-    $price = mysqlPrep($_POST['price']);
+    $price = (isset($_POST['is_free'])) ? '00.00' : mysqlPrep($_POST['price']);
     $imagePath = mysqlPrep($_POST['image_path']);
+    $pros = mysqlPrep($_POST['pros']);
+
+    $datetime = explode('T', $_POST['datetime']);
+    $date = $datetime[0];
+    $time = $datetime[1];
 
     $sql = isset($_GET['id']) ?
-        "UPDATE events SET title = '{$title}', place = {$_POST['place']}, `datetime` = '{$_POST['datetime']}',
+        "UPDATE events SET title = '{$title}', place = {$_POST['place']}, `datetime` = '{$date} {$time}',
         description = '{$desc}', is_free = {$_POST['is_free']}, price = '{$price}', image_path = '{$imagePath}'
         WHERE id={$_GET['id']};"
         :
         "INSERT INTO events (title, place, datetime, description, is_free, price, image_path) 
-        VALUES ('{$title}', {$_POST['place']}, '{$_POST['datetime']}', '{$desc}', {$_POST['is_free']},
+        VALUES ('{$title}', {$_POST['place']}, '{$date}', '{$desc}', {$_POST['is_free']},
         '{$price}', '{$imagePath}');";
 
     if ($result = $mysqli->query($sql)) {
-        mysqli_free_result($result);
         $mysqli->close();
         redirect(server_root() . '/admin/admin.php');
     } else {
