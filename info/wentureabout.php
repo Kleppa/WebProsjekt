a8c0d0e3434eed59ffde89ad7369bd03c0a7af5b
B1tm4n <knut.a.abelseth@live.no>
2017-05-28 14:21:03 +0200

    info side om Wenture

---
+++ info/wentureabout.php
@@ -0,0 +1,45 @@
+<?php
+$pagetitle = 'about';
+require_once '../vendor/autoload.php';
+require_once '../private/phpscripts/db_connector.php';
+require_once '../private/phpscripts/functions.php';
+
+require '../private/includes/header.php'
+?>
+
+    <div class="container">
+        <div class="row margin-adder">
+
+            <div class="row justify-content-center no-gutters">
+                <div class="col-7">
+
+                    <div class="row justify-content-center">
+                        <h1>WENTURE</h1>
+                    </div>
+
+
+                        <div class="row">
+                            <div class="col justify-content-center">
+                                <p>Wenture is the creation we made for our school project. Our goal is to make an interactive website for the students at Westerdals, mapping the surrounding area around Westerdals campuses. </p>
+
+                                   <p> Our vision for Wenture is all about the experience students have off campus and when they are going out to explore the city. Our website is filled with the best locations around Westerdals, and with this we can show the students the best places to chill or have drink.
+                                   </p>
+
+                                    <p>We also have a rating system where you can up vote places you like, and when places receive the votes they will move up on the page. In that way, we keep the hottest locations on top, ready to be explored!
+                                </p>
+
+                                <p>If you’re out of luck with finding new places to visit, our handbook might be the thing for you.</p>
+
+                                <p>Bon voyage, Wenturer!</p>
+
+                            </div>
+                        </div>
+
+
+
+            </div>
+        </div>
+        </div>
+    </div>
+
+<?php require_once '../private/includes/footer.php';
